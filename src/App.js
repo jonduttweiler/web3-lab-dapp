ecd90abfa4714a2ba5a4460190b0adf8364e1cd9
jona <jonatanduttweiler@gmail.com>
2021-02-01 20:51:22 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import Web3 from 'web3';
+import { useEffect, useState } from 'react';
 import './App.css';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  height:100vh;
+  width:100%;
+  box-sizing:border-box;
+`
+const AppHeader = styled.div`
+  display:flex;
+  justify-content:center;
+  padding:20px;
+`
+const Row = styled.div`
+  padding: 5px;
+`
+
 
 function App() {
+  const [account, setAccount ] = useState();
+  const [balance, setBalance ] = useState();
+  const [netId, setNetId] = useState();
+  const [web3, setWeb3] = useState();
+
+  //EIP 1102 Connect method
+  useEffect(() => {
+    async function getAccounts(){
+      const response = await window.ethereum.request({method:'eth_requestAccounts'});
+      const accounts = response.result? response.result : response;
+      setAccount(accounts[0])
+    }
+    getAccounts();
+  },[])
+
+   useEffect(() => {
+
+    async function getNetId(){
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        
+        setWeb3(web3);
+        setNetId(await web3.eth.net.getId());
+        if(account){
+          setBalance(await web3.eth.getBalance(account));
+        }
+
+      }
+
+    }
+    getNetId();
+    
+
+
+  },[account]);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <AppHeader>Dapps Lab</AppHeader>
+      <Row>Account: {account}</Row>
+      <Row>Net id: {netId}</Row>
+    </AppWrapper>
   );
 }
 
