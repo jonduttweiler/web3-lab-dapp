81a1f599ab7824d5f3129c808d52c3bc841b2917
jona <jonatanduttweiler@gmail.com>
2021-02-04 20:21:58 -0300

    Move styles

--- src/components/CrowdfundingLab.js
+++ src/components/CrowdfundingLab.js
@@ -5,56 +5,8 @@ import { CircularProgress } from "@material-ui/core";
 import { useEffect, useState } from "react";
 
 import EntityCard from "./cards/EntityCard";
+import { MethodsWrapper, Row, Chip, DLabButton, AddressInput } from './styled/CrowdfundingLab';
 
-
-const MethodsWrapper = styled.div`
-  padding: 10px;
-  display: flex;
-  flex-direction: column;
-  flex-grow: 0;
-`;
-const Row = styled.div`
-  display: flex;
-  align-items: center;
-`;
-const Chip = styled.div`
-  border: 2px solid #3584D4;
-  border-radius: 20px;
-  padding: 0px 8px;
-  font-weight: bold;
-  text-align: center;
-  color: white;
-  background-color: #3584D4;
-  margin: 0px 3px;
-`;
-const DLabButton = styled.div`
-  cursor: pointer;
-  border: 2px solid tomato;
-  padding: 5px 10px;
-  margin: 10px;
-  border-radius: 50px;
-  color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items:center;
-  max-width: 150px;
-
-  :hover {
-    background-color: tomato;
-    color: white;
-  }
-`;
-
-const AddressInput = styled.input`
-  ${(props) => {
-    if (props.invalidAddress) {
-      return "border:1px solid red;";
-    } else {
-      return "border:1px solid gray;";
-    }
-  }}
-  width:100%;
-`;
 
 const ErrorWrapper = styled.div`
   border: 2px solid red;
@@ -93,7 +45,6 @@ function CrowdfundingLab() {
       const crowdfunding = new web3.eth.Contract(CrowdfundingAbi, address);
       setCrowdfunding(crowdfunding);
       const methods = Object.keys(crowdfunding.methods);
-      console.log(methods);
       setMethods(methods);
     } catch (err) {
       console.log("invalid address");
---
+++ src/components/styled/CrowdfundingLab.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const MethodsWrapper = styled.div`
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 0;
+`;
+export const Row = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const Chip = styled.div`
+  border: 2px solid #3584D4;
+  border-radius: 20px;
+  padding: 0px 8px;
+  font-weight: bold;
+  text-align: center;
+  color: white;
+  background-color: #3584D4;
+  margin: 0px 3px;
+`;
+export const DLabButton = styled.div`
+  cursor: pointer;
+  border: 2px solid tomato;
+  padding: 5px 10px;
+  margin: 10px;
+  border-radius: 50px;
+  color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  max-width: 150px;
+
+  :hover {
+    background-color: tomato;
+    color: white;
+  }
+`;
+
+export const AddressInput = styled.input`
+  ${(props) => {
+    if (props.invalidAddress) {
+      return "border:1px solid red;";
+    } else {
+      return "border:1px solid gray;";
+    }
+  }}
+  width:100%;
+`;
